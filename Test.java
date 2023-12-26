29266eae64db1c4dd57e1b43a3106d7716781199
Saransh Sharma <saranshsharma2212.com>
2023-12-26 14:53:07 +0530

    Java
    Everything i have learnt and practiced with Java

---
+++ Test.java
@@ -0,0 +1,36 @@
+class Test{
+    public static void main(String[] args){
+        int x = 0;
+        int y = 0;
+
+        while(x < 5){
+        //candidate code goes here 
+            //candidate 1 : y = x - y;
+
+            //candidate 2 : y = x + y;
+
+            // y = y + 2;
+            // if(y > 4){
+            //     y = y - 1;
+            // }
+
+            // x = x + 1;
+            // y = y + x;
+
+            if(y < 5){
+                x = x + 1;
+                if(y<3){
+                    x = x -1;
+                }
+            }
+
+            y = y +2;
+
+
+        ///////////////
+
+            System.out.print(x + "" + y + " ");
+            x = x + 1;
+        }
+    }
+}
---
+++ myFirstApp.java
@@ -0,0 +1,6 @@
+public class myFirstApp{
+    public static void main(String[] args){
+        System.out.print("I Rule!\n");
+        System.out.print("The World");
+    }
+}
