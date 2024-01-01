2c66eef21ed40ce6a2bc0ae8a00bdce3da894c03
Saransh Sharma <Saranshsharma2212@gmail.com>
2024-01-01 13:48:36 +0530

    Arrays

--- Dog.java
+++ Dog.java
@@ -1,9 +1,53 @@
 class Dog{
-    int size;
-    String breed;
     String name;
+    int size;
 
-    void bark(){
-        System.out.println("Ruff! Ruff!");
+    public static void main(String[] args){
+        Dog dog1 = new Dog();
+        dog1.bark(1);
+        dog1.name = "Bart";
+
+        Dog[] myDogs = new Dog[3];
+
+        myDogs[0] = new Dog();
+        myDogs[1] = new Dog();
+        myDogs[2] = dog1;
+
+
+        myDogs[0].name = "Fred";
+        myDogs[1].name = "Marge";
+
+        System.out.print("Last dog's name is ");
+        System.out.println(myDogs[2].name);
+
+        int x = 0;
+
+        while(x < myDogs.length){
+            myDogs[x].bark(1);
+            x += 1;
+        }
+    }
+
+    void bark(int numOfBarks){
+        while(numOfBarks > 0){
+
+        if(size > 60){
+            System.out.println("Woof! Woof!");
+        }else if(size > 14) {
+            System.out.println("Ruff! Ruff!");
+        }else{
+            System.out.println("Yip! Yip!");
+        }
+        numOfBarks -= 1;
+    }
+
+    }
+
+    void chaseCat(){
+        System.out.println(name + " is chasing cats!");
+    }
+
+    void eat(){
+        System.out.println(name + " is eating.");
     }
 }
--- DogTestDrive.java
+++ DogTestDrive.java
@@ -1,7 +1,21 @@
 class DogTestDrive{
     public static void main(String[] args){
+
+        Dog one = new Dog();
+        one.size = 70;
+        
+        Dog two = new Dog();
+        two.size = 8;
+
+        Dog three = new Dog();
+        three.size = 35;
+
+        one.bark(1);
+        two.bark(1);
+        three.bark(1);
+
+        
         Dog d = new Dog();
-        d.size = 40;
-        d.bark();
+        d.bark(3);
     }
 }
