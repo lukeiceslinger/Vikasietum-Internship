afc9326da5628d4bf2827ef04ecd3e167b998dba
Saransh Sharma <Saranshsharma2212@gmail.com>
2024-01-18 14:34:04 +0530

    Improved DotCom

--- DotComBust.java
+++ DotComBust.java
@@ -1,12 +1,14 @@
-public class SimpleDotComGame {
+import java.util.ArrayList;
+
+public class DotComBust {
     public static void main(String[] args){
         int numOfGuesses = 0;
 
         GameHelper helper = new GameHelper();
-        SimpleDotCom theDotCom = new SimpleDotCom();
+        DotCom theDotCom = new DotCom();
 
         int randomNum =(int) (Math.random() * 5);
-        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        ArrayList<String> locations = new ArrayList<>();
         theDotCom.setLocationCells(locations);
 
         boolean isAlive = true;
---
+++ DotComTestDrive.java
@@ -0,0 +1,9 @@
+public class DotComTestDrive {
+    public static void main(String[] args){
+        DotCom dot = new DotCom();
+
+        String userGuess = "2";
+
+        String result = dot.checkYourself(userGuess);
+    }
+}
