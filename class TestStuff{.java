1fad4a46c65364fabd0895d5b79ee48c2b5a5d1d
Saransh Sharma <Saranshsharma2212@gmail.com>
2024-01-18 17:18:05 +0530

    gitIgnore

---
+++ class TestStuff{.java
@@ -0,0 +1,12 @@
+class TestStuff{
+    
+    void go(){
+        TestStuff t = new TestStuff();
+        t.takeTwo(12, 34);
+    }
+
+    void takeTwo(int x, int y){
+        int z = x + y;
+        System.out.println("Total is " + z);
+    }
+}
